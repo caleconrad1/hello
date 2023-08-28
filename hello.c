8c1923527bdb81bd2c2ed5a0712006972f8ae8c6
caleconrad1 <cconrad6@huskers.unl.edu>
2023-08-28 18:18:55 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Cale Conrad
+ *         cconrad6@huskers.unl.edu
+ * Date: 2023/8/28
+ *
+ * Prints Hello World in C
+ *
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Cale Conrad\n");
+
+    return 0;
+}
